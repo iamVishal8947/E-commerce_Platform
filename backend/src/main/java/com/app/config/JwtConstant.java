12ea12603e8905e3070103cf204d2af393a38ce6
iamVishal8947 <jadhavvishal8947@gmail.com>
2024-02-28 00:58:46 +0530

    Basic JWt  implementation

---
+++ backend/src/main/java/com/app/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.app.config;
+
+public class JwtConstant {
+	public static final String SECRET_KEY="fjfkweroierwnjkfnmdnvkjlbvifsdjkfhk";
+	public static final String JWT_HEADER="Authoriztion";
+
+}
