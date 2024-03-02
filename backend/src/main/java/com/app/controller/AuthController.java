33e4fd03072c2413797c38de80d54de7f7f3001c
iamVishal8947 <jadhavvishal8947@gmail.com>
2024-03-02 07:01:39 +0530

    Complete frontend

--- backend/src/main/java/com/app/controller/AuthController.java
+++ backend/src/main/java/com/app/controller/AuthController.java
@@ -2,9 +2,11 @@ package com.app.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,7 @@ import com.app.exception.UserException;
 import com.app.repository.UserRepository;
 import com.app.request.LoginRequest;
 import com.app.responce.AuthResponse;
+import com.app.service.CustomUserDetails;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,9 +28,14 @@ public class AuthController {
 	private UserRepository userRepositroy;
 	private JwtProvider jwtProvider;
 	private PasswordEncoder passwordEncoder;
+	private CustomUserDetails customUserDetails;
 	
-	public AuthController(UserRepository userRepositroy) {
-		this.userRepositroy=userRepositroy;
+	public AuthController(UserRepository userRepository,PasswordEncoder passwordEncoder,JwtProvider jwtProvider,CustomUserDetails customUserDetails) {
+		this.userRepositroy=userRepository;
+		this.passwordEncoder=passwordEncoder;
+		this.jwtProvider=jwtProvider;
+		this.customUserDetails=customUserDetails;
+		
 	}
 	
 	@PostMapping("/SignUp")
@@ -59,10 +67,41 @@ public class AuthController {
 		return new ResponseEntity<AuthResponse>(authResponce, HttpStatus.CREATED);
 	}
 	
-	public ResponseEntity<AuthResponse> loginUserHandler(@RequestBody LoginRequest loginRequest){
+	@PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        
+        System.out.println(username +" ----- "+password);
+        
+        Authentication authentication = authenticate(username, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        
+        String token = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse= new AuthResponse();
 		
-		return null;
-	}
+		authResponse.setStatus(true);
+		authResponse.setJwt(token);
+		
+        return new ResponseEntity<AuthResponse>(authResponse,HttpStatus.OK);
+    }
+	
+	private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+        
+        System.out.println("sign in userDetails - "+userDetails);
+        
+        if (userDetails == null) {
+        	System.out.println("sign in userDetails - null " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+        	System.out.println("sign in userDetails - password not match " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 	
 	
 
